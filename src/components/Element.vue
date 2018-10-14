060e38c2bab00f1a03802c553a2e6b6da57691eb
Eric Arbour <arboureric@gmail.com>
2018-10-13 20:26:50 -0400

    initial commit

---
+++ src/components/Element.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <div class="content">
+      {{ element.number }}
+      {{ element.symbol }}
+      {{ element.name }}
+      {{ element.atomic_mass }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Element",
+  props: {
+    element: Object
+  }
+};
+</script>
---
+++ src/components/PeriodicTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>The Periodic Table</h1>
+    <div class="ui cards">
+      <Element 
+        v-for="element in elements"
+        :key="element.name"
+        :element="element"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import elements from "../data/elements";
+import Element from "./Element.vue";
+
+console.log(elements);
+
+export default {
+  name: "PeriodicTable",
+  components: {
+    Element
+  },
+  data: function() {
+    return {
+      elements
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
